22b2666294e6dffbdd3dad6be6e4c7879adb4a04
taha_dehghan <118622067+tahadev123@users.noreply.github.com>
2025-06-15 19:39:51 +0330

    Update TitleAnimation.tsx

--- src/elements/TitleAnimation.tsx
+++ src/elements/TitleAnimation.tsx
@@ -17,7 +17,7 @@ function TitleAnimation({ hasAnimated }: Props) {
       className="w-full max-w-screen-sm flex justify-center z-10 px-4"
     >
       <div className="w-full flex justify-center z-10">
-        <div className="flex flex-wrap gap-x-1 mb-5 text-white font-bold leading-relaxed text-[20px] sm:text-[33px]">
+        <div className="flex flex-wrap gap-x-1 mb-5 text-white text-center font-bold leading-relaxed text-[20px] sm:text-[33px]">
           {words.map((word, i) => (
             <motion.span
               key={i}
